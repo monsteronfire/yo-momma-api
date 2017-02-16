05826cd4236e853439638ea8a85d5361c2a2c2f0
monsteronfire <monsteronfire@gmail.com>
2017-02-16 09:27:08 +0800

    Add initializer for AMS

--- app/models/joke.rb
+++ app/models/joke.rb
@@ -1,2 +1,10 @@
 class Joke < ApplicationRecord
+  def index
+    @jokes = Joke.all
+    render json: @jokes
+  end
+
+  def show
+    render json: @animal
+  end
 end
---
+++ config/initializers/active_model_serializer.rb
@@ -0,0 +1,3 @@
+# config/initializers/active_model_serializer.rb
+#ActiveModel::Serializer.config.adapter = ActiveModel::Serializer::Adapter::JsonApi
+ActiveModelSerializers.config.adapter = :json_api
